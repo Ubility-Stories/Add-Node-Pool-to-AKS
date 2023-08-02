10aa2083d027de805ad34c7ac711f95f8ca2343e
ibrahim nasri <ibrahimnasri24@gmail.com>
2023-08-02 11:23:50 +0300

    added cred variables

--- providers.tf
+++ providers.tf
@@ -11,4 +11,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  client_id       = var.CLIENT_ID
+  client_secret   = var.SECRET
+  tenant_id       = var.TENANT_ID
+  subscription_id = var.SUBSCRIPTION_ID
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,22 @@
+variable "CLIENT_ID" {
+  type      = string
+  sensitive = true
+}
+
+variable "SECRET" {
+  type      = string
+  sensitive = true
+}
+
+variable "SUBSCRIPTION_ID" {
+  type      = string
+  sensitive = true
+}
+variable "TENANT_ID" {
+  type      = string
+  sensitive = true
+}
+
 variable "node-pool-name" {
   type    = string
   default = "pool"
